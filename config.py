b25e5c99b2ca68be74e392c276cf5a36bb7e551b
Stuart MacGowan <smacgowan@dundee.ac.uk>
2024-12-14 00:56:52 +0000

    Refactor config paths for improved data handling and environment compatibility

--- config.py
+++ config.py
@@ -4,32 +4,28 @@ import os
 URL_PREFIX = os.environ.get('URL_PREFIX', '/ligysis')
 STATIC_URL_PATH = f"{URL_PREFIX}/static"
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # VALID FOR ALL (UNUSED IN CLUSTER)
+# Base directory for session and data storage (including example data)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# DATA_FOLDER = os.path.join(BASE_DIR, "static", "data") # IN LOCAL & EXAMPLE
-DATA_FOLDER = "/Volumes/2394007/LIGYSIS_PDB/" # IN LOCAL (after connecting with SAMBA to /cluster/gjb_lab/2394007)
-#DATA_FOLDER = "/cluster/gjb_lab/2394007/LIGYSIS_PDB" # IN CLUSTER
-
-# SPLIT_FOLDER = BIOLIP_FOLDER # IN EXAMPLE
-# SPLIT_FOLDER = os.path.join(DATA_FOLDER, "biolip", "split") # IN LOCAL
-# SPLIT_FOLDER = DATA_FOLDER # IN CLUSTER
+# /path/to/LIGYSIS_PDB
+EXAMPLES = os.path.join(BASE_DIR, "static", "data")
+DATA_FOLDER = os.environ.get('APP_DATA_PATH', EXAMPLES)
+if DATA_FOLDER == EXAMPLES:
+    SPLIT_FOLDER = os.path.join(DATA_FOLDER, "biolip", "split")
+else:
+    SPLIT_FOLDER = DATA_FOLDER
 
 ASSEMBLY_FOLDER = os.path.join(DATA_FOLDER, "data", "structures", "assembly") # IN EXAMPLE & CLUSTER
 CIF_SIFTS_DIR = os.path.join(DATA_FOLDER, "data", "cif_sifts") # IN CLUSTER
 CHAIN_MAPPING_DIR = os.path.join(DATA_FOLDER, "data", "chain_remapping") # IN CLUSTER
 
-PROTS_FOLDER = os.path.join(DATA_FOLDER, "output_V2") # IN EXAMPLE & LOCAL
+PROTS_FOLDER = os.path.join(DATA_FOLDER, "output_V2")
 
-# SITE_TABLES_FOLDER = os.path.join(SPLIT_FOLDER, "site_tables") # IN EXAMPLE & LOCAL
-SITE_TABLES_FOLDER = os.path.join(DATA_FOLDER, "site_tables") # IN CLUSTER
-#SITE_TABLES_FOLDER = os.path.join(DATA_FOLDER, "site_tables") # IN CLUSTER
+# Paths that can be resolved when with DATA_FOLDER or SPLIT_FOLDER
+SITE_TABLES_FOLDER = os.path.join(SPLIT_FOLDER, "site_tables")
+RES_TABLES_FOLDER = os.path.join(SPLIT_FOLDER, "res_tables")
+REP_STRUCS_FOLDER = os.path.join(SPLIT_FOLDER,  "segment_coords")
 
-# RES_TABLES_FOLDER = os.path.join(SPLIT_FOLDER, "res_tables") # IN EXAMPLE & LOCAL
-RES_TABLES_FOLDER = os.path.join(DATA_FOLDER,  "res_tables") # IN CLUSTER
-#RES_TABLES_FOLDER = os.path.join(DATA_FOLDER, "res_tables") # IN CLUSTER
-
-# REP_STRUCS_FOLDER = os.path.join(SPLIT_FOLDER, "segment_rep_dicts") # IN EXAMPLE & LOCAL
-REP_STRUCS_FOLDER = os.path.join(DATA_FOLDER,  "segment_coords")
 #REP_STRUCS_FOLDER = os.path.join(DATA_FOLDER, "segment_rep_dicts") # IN CLUSTER
 
 # USER_JOBS_FOLDER = os.path.join(BASE_DIR, "static", "data", "USER_JOBS") # THIS WILL NEED TO CHANGE WHEN MOVED TO CLUSTER
@@ -40,8 +36,6 @@ USER_JOBS_IN_FOLDER = os.path.join(USER_JOBS_FOLDER, "IN") # THIS WILL NEED TO C
 USER_JOBS_OUT_FOLDER = os.path.join(USER_JOBS_FOLDER, "OUT") # THIS WILL NEED TO CHANGE WHEN MOVED TO CLUSTER
 
 # Config for users submission handling
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 SESSIONS_FOLDER = os.path.join(BASE_DIR, 'sessions')
 SESSIONS_FOLDER = os.environ.get('APP_SESSIONS_PATH', SESSIONS_FOLDER)
 
