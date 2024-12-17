b645c39ecb44d112b4b197305a1ecc6190668c7f
JavierSanchez-Utges <capitan_xavi@hotmail.com>
2024-12-17 10:20:34 +0100

    Fixed PyMol ALL structured download for user job results

--- app.py
+++ app.py
@@ -2432,9 +2432,12 @@ def user_download_all_structures_PyMol(): # route to download PyMol scripts to v
     data = request.get_json() # Get JSON data from the POST request
     
     job_id = data.get('jobId')
+    session_id = data.get('sessionId')
+    submission_time = data.get('submissionTime')
     assembly_pdb_ids = data.get('assemblyPdbIds')  # This is your array
 
-    job_output_dir = os.path.join(USER_JOBS_OUT_FOLDER, job_id)
+    # job_output_dir = os.path.join(USER_JOBS_OUT_FOLDER, job_id)
+    job_output_dir = os.path.join(SESSIONS_FOLDER, session_id, submission_time, "OUT", job_id)
     job_supp_cifs_dir = os.path.join(job_output_dir, "supp_cifs")
     job_arpeggio_dir = os.path.join(job_output_dir, "arpeggio")
     job_results_dir = os.path.join(job_output_dir, "results")
