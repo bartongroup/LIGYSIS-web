9bc971c707a9c270360988eb085c64130206da56
JavierSanchez-Utges <capitan_xavi@hotmail.com>
2025-04-14 17:26:24 +0100

    Fixed binding site rows selection from table and fixed change segment URL

--- static/js/change_segment.js
+++ static/js/change_segment.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     $('#selectSegment').on('change', function() {
         var selectedValue = $(this).val();
         if (selectedValue) {
-        window.location.href = selectedValue; // Redirect to the selected page (structural segment)
+            //console.log(window.location.href); // Log the current URL
+            //console.log(selectedValue); // Log the selected value
+            window.location.href = window.appBaseUrl + selectedValue; // Redirect to the selected page (structural segment)
         }
     });
 });
--- static/js/charts.js
+++ static/js/charts.js
@@ -309,7 +309,7 @@ let newChartConfig = { // configuration for the new chart
                 callbacks: {
                     title: function(context) {
                         let dataIndex = context[0].dataIndex; // Assuming you are only hovering over a single point
-                        return newChartData[newChartLab][dataIndex]; // Use the 'UniProt_ResNum' property from your data
+                        return "Residue " + newChartData[newChartLab][dataIndex]; // Use the 'UniProt_ResNum' property from your data
                     },
                 }
             },
